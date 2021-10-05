200750bb964a446cf3ab55f5686e3ddcb7df8d0b
Ammar Qammaz <ammarkov@gmail.com>
2021-10-05 14:01:59 +0300

    adding a script to run robot stuff..

---
+++ scripts/runRobots.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+cd ..
+cd ..
+
+source ../devel/setup.bash
+
+screen -S RAISIM -d -m roslaunch raisim_ros nao.launch 
+screen -S IK -d -m roslaunch whole_body_ik nao_wbc.launch
+screen -S ACTIONCLIENT -d -m rosrun  action_client_test pose_control.py
+ 
+
+screen -S CAMERA -d -m roslaunch rgbd_acquisition rgb_acquisition.launch moduleID:=V4L2 deviceID:=/dev/video0 width:=640 height:=480 framerate:=30
+screen -S MOCAPNET -d -m roslaunch mocapnet_rosnode mocapnet_rosnode.launch
+
+
+screen -list
+
+
+
+
+
+
+
+
+
+
+
+exit 0
