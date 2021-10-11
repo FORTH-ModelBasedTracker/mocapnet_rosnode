49ac2cf2876aa2a1273087653da7f23b41fcc040
Ammar Qammaz <ammarkov@gmail.com>
2021-10-11 18:00:23 +0300

    ...

--- scripts/runRobots.sh
+++ scripts/runRobots.sh
@@ -14,7 +14,7 @@ screen -S RAISIM -d -m roslaunch raisim_ros nao.launch
 screen -S IK -d -m roslaunch whole_body_ik nao_wbc.launch
 screen -S ACTIONCLIENT -d -m rosrun  action_client_test pose_control.py
  
-
+#screen -S CAMERA -d -m roslaunch rgbd_acquisition rgb_acquisition.launch moduleID:=TEMPLATE deviceID:=sven.mp4-data width:=1920 height:=1080 framerate:=30
 screen -S CAMERA -d -m roslaunch rgbd_acquisition rgb_acquisition.launch moduleID:=V4L2 deviceID:=/dev/video0 width:=640 height:=480 framerate:=30
 screen -S MOCAPNET -d -m roslaunch mocapnet_rosnode mocapnet_rosnode.launch
 
